84f79487dfbb191dee375cf0590af7c8b8ca3163
Thomaskutty Reji <65359305+thomasreji155@users.noreply.github.com>
2021-08-25 22:52:41 +0530

    Add files via upload

--- tf_idf.py
+++ tf_idf.py
@@ -1,46 +1,40 @@
 
+'''
+TF-IDF method
+========================================
+it shows how relevant a word in a series or corpus is to a text. 
+TF-IDF is a frequency-based method that takes into account the frequency with
+which a word appears in a corpus. This is a word representation in the sense that it
+represents the importance of a specific word in a given document. Intuitively, the
+higher the frequency of the word, the more important that word is in the document.
 
+TF stands for term frequency and IDF stands for inverse document frequency:
 
-paragraph = '''
-I have three visions for India. In 3000 years of our history, people from all over 
-the world have come and invaded us, captuour lands, conquered our minds. 
-From Alexander onwards, the Greeks, the Turthe Moguls, the Portuguese, the British,
-the French, the Dutch, all of them came looted us, took over what was ours. 
-Yet we have not done this to any other natiWe have not conquered anyone. 
+TF(wi) = number of times wi appear / total number of words
+IDF(wi) = log(total number of documents / number of documents with wi in it)
+
+TF-IDF(wi) = TF(wi) x IDF(wi)
+
+for example :
+ sentence 1 = 'This is about cats. Cats are great companions'
+ sentence 2 = 'This is about dogs. Dogs are very loyal'
+
+tf-idf(cats,sentenc1) = (2/8) * log(2/1) = 0.075
+tf-idf(this, senetence2) = (1/8) * log(2/2) = 0.0
 '''
 
-import re 
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+corpus = ['This is about cats. cats are great companions',
+          'This is about dogs. dogs are very loyal']
 
+tfidf = TfidfVectorizer()
+tfidf_vector = tfidf.fit_transform(corpus)
+result = tfidf.fit_transform(corpus).toarray()
 
+result_arr = np.round(result, decimals=2).T
 
-ps = PorterStemmer()
-wordnet = WordNetLemmatizer()
-sentences = nltk.sent_tokenize(paragraph)
-corpus = []
-for i in range(len(sentences)):
-    english_sw = stopwords.words('english')
-    review = re.sub('[^a-zA-Z]', ' ',sentences[i]) 
-    review = review.lower() 
-    review = review.split()
-    review = [wordnet.lemmatize(word) for word in review if not word in set(english_sw)]
+feature_names = tfidf.get_feature_names()
 
-    review = ' '.join(review) 
-    corpus.append(review) 
-
-from sklearn.feature_extraction.text import TfidfVectorizer 
-cv = TfidfVectorizer()
-X = cv.fit_transform(corpus).toarray() 
-
-print()
-print(sentences[0:4]) 
-print(corpus[0:5]) 
-
-print()
-print(X)
-
-
+for name, value in zip(feature_names, result_arr):
+    print(name, value)
